419383cb7483e3d835d383301963c39c0ca3dacd
andrey_velsk <andrey_velsk@mail.ru>
2018-12-25 22:56:01 +0300

    add 404

--- pages/main.php
+++ pages/main.php
@@ -1,4 +1,6 @@
-
+<?php
+    $city_file = "pages/$city.php";
+?>
 <div class="mainview container">
     <div class="row">
         <div class="col-lg-3">
@@ -9,7 +11,10 @@
 
         <div class="content col-lg-9">
             <?php
-            include "pages/$city.php"; // переход на страницу города по переменной city
+            if(file_exists($city_file)) // если файл существует
+                include "pages/$city.php"; // переход на страницу города по переменной city
+            else
+                include "pages/404.php"; // если файла нет, ошибка 404
             ?>
         </div>
     </div>
